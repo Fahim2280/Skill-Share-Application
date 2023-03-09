8241c5ad66ef6d3da9d522c3d2a7faac76eb3ec7
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2023-03-10 02:04:28 +0600

    change

--- Controller/Search.php
+++ Controller/Search.php
@@ -1,15 +1,68 @@
-<?php
-$search_query = $_POST['search'];
-$file_path = '../Model/course.txt';
-$file = fopen($file_path, 'r');
+<!DOCTYPE html>
+<html>
 
-if ($file) {
-    while (($line = fgets($file)) !== false) {
-        if (strpos($line, $search_query) !== false) {
-            echo '<p>' . htmlspecialchars($line) . '</p>'; // Display the line
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
         }
+
+        h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .course-info {
+            display: flex;
+            flex-wrap: wrap;
+            margin: 0 -10px;
+        }
+
+        .course-info p {
+            flex: 1;
+            padding: 10px;
+            background-color: #f5f5f5;
+            border: 1px solid #ccc;
+        }
+
+        .course-info p:first-child {
+            flex-basis: 100%;
+            background-color: #ddd;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $search_query = $_POST['search'];
+    $file_path = '../Model/Course.txt';
+    $file = fopen($file_path, 'r');
+
+    if ($file) {
+        while (($line = fgets($file)) !== false) {
+            if (strpos($line, $search_query) !== false) {
+                $data = explode('|', $line);
+                echo '<p>ID: ' . htmlspecialchars($data[0]) . '</p>';
+                echo '<p>Course Name: ' . htmlspecialchars($data[1]) . '</p>';
+                echo '<p>Course Code: ' . htmlspecialchars($data[2]) . '</p>';
+                echo '<p>Duration: ' . htmlspecialchars($data[3]) . '</p>';
+                echo '<p>Fee: ' . htmlspecialchars($data[4]) . '</p>';
+                echo '<p>Level: ' . htmlspecialchars($data[5]) . '</p>';
+                echo '<p>Teacher: ' . htmlspecialchars($data[6]) . '</p>';
+                echo '<p>Images: ' . htmlspecialchars($data[7]) . ', ' . htmlspecialchars($data[8]) . '</p>';
+                echo '<p>Rating: ' . htmlspecialchars($data[9]) . '</p>';
+                echo '<p>Status: ' . htmlspecialchars($data[10]) . '</p>';
+                break;
+            }
+        }
+        fclose($file);
+    } else {
+        echo 'Unable to open file.';
     }
-    fclose($file);
-} else {
-    echo 'Unable to open file.';
-}
+    ?>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -16,25 +16,37 @@
     session_start();
 
     // Read course information from the text file
-    $files = file("./Model/Course.txt");
-
-    foreach ($files as $line) {
+    $file = fopen("./Model/Course.txt", "r") or die("Unable to open file!");
+    while (!feof($file)) {
+        $line = fgets($file);
         $course_filed = explode('|', $line);
+        $course_id = $course_filed[0];
         $course_title = $course_filed[1];
         $course_description = $course_filed[2];
-        $instructor_name = $course_filed[6];
+        $duration = $course_filed[3];
         $price = $course_filed[4];
-        $Duration = $course_filed[3];
-        $Reting = $course_filed[9];
+        $level = $course_filed[5];
+        $instructor_name = $course_filed[6];
+        $image1 = $course_filed[7];
+        $image2 = $course_filed[8];
+        $rating = $course_filed[9];
+        $status = $course_filed[10];
 
         $courses[] = array(
+            "id" => $course_id,
             "title" => $course_title,
             "description" => $course_description,
+            "duration" => $duration,
+            "price" => $price,
+            "level" => $level,
             "instructor" => $instructor_name,
-            "price" => $price
+            "image1" => $image1,
+            "image2" => $image2,
+            "rating" => $rating,
+            "status" => $status
         );
     }
-
+    fclose($file);
 
     if (isset($_SESSION['email'])) {
         // Loop through the courses array using a foreach loop
@@ -45,31 +57,31 @@
                     <img class="course-image" src="course-image.jpg" alt="Course Image">
                 </div>
                 <div class="course-details">
-                    <h2 class="course-title" name="title"><?php echo $course_title; ?></h2>
-                    <p class="course-description"><?php echo $course_description; ?></p>
-                    <p class="course-instructor">Instructor: <?php echo $instructor_name; ?></p>
-                    <p class="course-duration">Duration: <?php echo $Duration; ?></p>
-                    <p class="course-rating">Rating: <?php echo $Reting; ?></p>
-                    <p class="course-price">Price: <?php echo $price; ?></p>
+                    <h2 class="course-title" name="title"><?php echo $course['title']; ?></h2>
+                    <p class="course-description"><?php echo $course['description']; ?></p>
+                    <p class="course-instructor">Instructor: <?php echo $course['instructor']; ?></p>
+                    <p class="course-duration">Duration: <?php echo $course['duration']; ?></p>
+                    <p class="course-rating">Rating: <?php echo $course['rating']; ?></p>
+                    <p class="course-price">Price: <?php echo $course['price']; ?></p>
                     <form action="./Controller/enroll.php" method="post">
-                        <input type="hidden" name="course" value="<?php echo $course_title; ?>">
-                        <input type="hidden" name="instructor_name" value="<?php echo $instructor_name; ?>">
+                        <input type="hidden" name="course" value="<?php echo $course['title']; ?>">
+                        <input type="hidden" name="instructor_name" value="<?php echo $course['instructor']; ?>">
                         <input type="hidden" name="username" value="<?php echo $_SESSION['email']; ?>">
                         <button type="submit" name="enroll" class="enroll-button">Enroll Now</button>
+                    </form>
                 </div>
             </div>
     <?php
         }
     } else {
         echo "<p>Please login to view courses.</p>";
-    } ?>
-
+    }
+    ?>
     <div>
         <?php
         include './View/footer.php';
         ?>
     </div>
-
 </body>
 
 </html>
